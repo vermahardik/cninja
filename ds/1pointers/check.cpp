a9971d232a9141557e528040f35900b52574c53c
vermahardik <82748478+vermahardik@users.noreply.github.com>
2021-05-06 11:05:20 +0530

    6may

--- ds/1pointers/check.cpp
+++ ds/1pointers/check.cpp
@@ -1,10 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-float f = 10.5;
-float p = 2.5;
-float* ptr = &f;
-(*ptr)++;
-*ptr = p;
-cout << *ptr << " " << f << " " << p;
+char s[]= "hello";
+char *p = s;
+cout << s[0] << " " << p[0];
 }
